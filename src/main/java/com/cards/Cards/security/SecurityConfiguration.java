cfb7209e55d1557ce55b8f61e98d9278862bf06f
Aamir Nazir Kuchay <aamir1kuchay@gmail.com>
2024-07-12 16:41:31 +0530

    frhejgfhyew

--- src/main/java/com/cards/Cards/security/SecurityConfiguration.java
+++ src/main/java/com/cards/Cards/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.cards.Cards.security;
 
 
 import com.cards.Cards.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
